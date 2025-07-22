d2fa47eac01c7d1d59bb1162ac2f2dfa610405b9
elmehdi waddad <ewaddad@cofarma.co.ma>
2025-07-22 15:39:04 +0100

    commit1

---
+++ GestionClient/Controllers/ClientController.cs
@@ -0,0 +1,30 @@
+﻿using GestionClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GestionClient.Controllers
+{
+    public class ClientController : Controller
+    {
+        // Pour stocker temporairement la liste (en vrai projet: utiliser une base de données)
+        public static List<Client> Clients = new List<Client>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(Client client)
+        {
+            Clients.Add(client);
+            return View("Save", client); // Affiche les infos du client ajouté
+        }
+
+        // Pour voir la liste complète
+        public IActionResult Liste()
+        {
+            return View(Clients);
+        }
+    }
+}
